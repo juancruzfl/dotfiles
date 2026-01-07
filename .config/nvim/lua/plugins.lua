ff380b75f39d51fd0d9aa205ac1691d9a2b1405a
Juan Cruz <cruzjuan298@gmail.com>
2026-01-06 23:53:20 -0500

    update: ghostty layout, keybings, and setting; add: nvim plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -368,7 +368,23 @@ require("lazy").setup({
                 })
             end,
         },
-
+        {
+            "sphamba/smear-cursor.nvim",
+              opts = {
+                smear_between_buffers = true,
+                smear_between_neighbor_lines = true,
+                scroll_buffer_space = true,
+                legacy_computing_symbols_support = false,
+                smear_insert_mode = true,
+                stiffness = 0.8,
+                trailing_stiffness = 0.6,
+                stiffness_insert_mode = 0.7,
+                trailing_stiffness_insert_mode = 0.7,
+                damping = 0.95,
+                daming_insert_mode = 0.95,
+                distance_stop_animating = 0.5,
+            },
+        }
 
     },
   checker = { enabled = true },
